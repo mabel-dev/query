5efdd7932e67fe6e64ea71b94ba507123547b831
Justin Joyce <justin.joyce@joocer.com>
2021-09-25 23:19:32 +0100

    fix

--- src/main.py
+++ src/main.py
@@ -84,7 +84,7 @@ application.mount(
 
 def serialize_response(response, max_records):
     i = -1
-    for i, record in enumerate(iter(response)):
+    for i, record in enumerate(response):
         if i == max_records:
             break
         if hasattr(record, "mini"):
