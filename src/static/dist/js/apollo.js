64362ac4b5dc17fb580837737d67a602a100174f
joocer <justin.joyce@joocer.com>
2024-03-26 19:32:11 -0400

    suggest

--- src/static/dist/js/apollo.js
+++ src/static/dist/js/apollo.js
@@ -5,14 +5,35 @@ const apollo_api = new APIAdapter('apollo');
 
 document.getElementById("assistantButton").addEventListener("click", function () {
     let prompt = document.getElementById("assistantPrompt").value;
+    let suggestion_container = document.getElementById("assistantSuggestions");
 
-    apollo_api.get("v1/identify", null, { token: prompt }).then(data =>
+    apollo_api.get("v1/identify", null, { token: prompt }).then(data => {
 
-        console.log(data)
+        console.log(data);
 
-    ).catch(err => console.error(`Error occurred: ${err}`));
-})
+    
+        let type_suggestion = `
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Continue Exploring</h5>
+                <p class="card-text">
+                    ${data.token} is a ${data.class}
+                    <br />
+                    <code>${data.search}</code>
+                    <br />
+                    <button type="button" class="btn btn-tiny btn-primary" title="Load Query into Editor"><i class="fa-fw fa-solid fa-reply fa-rotate-90"></i></button>
+                    <button type="button" class="btn btn-tiny btn-success" title="Run Query"><i class="fa-fw fa-solid fa-play"></i></button>
+                </p>
+            </div>
+        </div>`;
 
+        suggestion_container.innerHTML = type_suggestion;
+    }
 
-
+    ).catch(err => {
+        suggestion_container.innerHTML = "";
+        console.error(`Error occurred: ${err}`);
+    }
+    );
+);
 
